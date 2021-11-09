884118daf3059b7df027f1c8b020cc66b1a19d96
Textspot <sachinkatarki1995@gmail.com>
2021-11-09 20:57:07 +0530

    Added:Welcome to Emp_Wage  Switch_case Programme

--- src/com/bridgelabz/empwage/EmployeeWageComp.java
+++ src/com/bridgelabz/empwage/EmployeeWageComp.java
@@ -3,23 +3,25 @@ package com.bridgelabz.empwage;
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-		 System.out.println("Welcome to the Employee Wage Computation Program !!");
+		  System.out.println("Welcome to the Employee Wage Computation Program !!");
 	        int EMP_RATE_PER_HOUR = 20;
 	        int IS_FULL_TIME = 1;
 	        int IS_PART_TIME = 2;
 	        int empHrs = 0;
 	        int empwage = 0;
 	        double empcheck = Math.floor(Math.random() * 10) % 3;
-	        if (empcheck == IS_FULL_TIME) {
-	            System.out.println("Employee is Full time ");
-	            empHrs = 8;
-	        }else if(empcheck==IS_PART_TIME){
-	            System.out.println("Employee is Part time");
-	            empHrs = 4;
-	        }
-	        else {
-	            System.out.println("Employee is Absent");
-	            empHrs = 0;
+	        switch ((int) empcheck) {
+	            case 2:
+	                System.out.println("Employee is Part time ");
+	                empHrs = 4;
+	                break;
+	            case 1:
+	                System.out.println("Employee is Full time ");
+	                empHrs = 8;
+	                break;
+	            default:
+	                System.out.println("Employee is Absent");
+	                empHrs = 0;
 	        }
 	        empwage = empHrs * EMP_RATE_PER_HOUR;
 	        System.out.println("Emp wage = " + empwage);
